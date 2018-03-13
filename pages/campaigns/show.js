90d112e952e9e9c460ff31e1d43da90a10d778ff
diop <fodesdiop@gmail.com>
2018-03-13 14:41:54 -0700

    Fix campaign typo

--- pages/campaigns/show.js
+++ pages/campaigns/show.js
@@ -6,7 +6,7 @@ class CampaignShow extends Component {
   static async getInitialProps(props) {
     const campaign = Campaign(props.query.address)
 
-    const summary = await campaing.methods.getSummary().call()
+    const summary = await campaign.methods.getSummary().call()
 
     console.log(summary)
 
